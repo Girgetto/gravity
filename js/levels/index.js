b91f41ec2ac27bf54420fc6a6149a195456ba781
Giorgio <33903092+Girgetto@users.noreply.github.com>
2025-10-09 10:48:08 +0200

    Revise level progression and expand stage roster (#9)

--- js/levels/index.js
+++ js/levels/index.js
@@ -1,30 +1,249 @@
 /* eslint-disable no-unused-vars */
-const level1 = {
-  goal: { posX: 100, posY: 100 },
-  planets: [{ posX: 500, posY: 500, radius: 50, density: Math.pow(10, 6) }],
-};
+const levelDefinitions = [
+  () => ({
+    goal: { posX: 300, posY: 300 },
+    planets: [],
+  }),
+  () => ({
+    goal: { posX: 900, posY: 400 },
+    planets: [
+      {
+        posX: 550,
+        posY: 400,
+        radius: 35,
+        density: Math.pow(10, 6) * 0.9,
+      },
+    ],
+  }),
+  () => ({
+    goal: { posX: 1200, posY: 150 },
+    planets: [
+      {
+        posX: 650,
+        posY: 420,
+        radius: 55,
+        density: Math.pow(10, 6) * 1.1,
+      },
+      {
+        posX: 820,
+        posY: 220,
+        radius: 28,
+        density: Math.pow(10, 6) * 0.8,
+      },
+    ],
+  }),
+  () => ({
+    goal: { posX: 1250, posY: 600 },
+    planets: [
+      {
+        posX: 700,
+        posY: 350,
+        radius: 42,
+        density: Math.pow(10, 6) * 1.2,
+      },
+      {
+        posX: 460,
+        posY: 520,
+        radius: 30,
+        density: Math.pow(10, 6) * 0.7,
+      },
+      {
+        posX: 900,
+        posY: 220,
+        radius: 24,
+        density: Math.pow(10, 6) * 1.4,
+      },
+    ],
+  }),
+  (ctx) => ({
+    goal: {
+      posX: ctx.canvas.width - 250,
+      posY: ctx.canvas.height / 2,
+    },
+    planets: [
+      {
+        posX: ctx.canvas.width / 2,
+        posY: ctx.canvas.height / 2,
+        radius: 60,
+        density: Math.pow(10, 6) * 1.5,
+      },
+      {
+        posX: ctx.canvas.width / 3,
+        posY: ctx.canvas.height / 3,
+        radius: 35,
+        density: Math.pow(10, 7) * 0.6,
+      },
+      {
+        posX: (ctx.canvas.width / 3) * 2,
+        posY: (ctx.canvas.height / 4) * 3,
+        radius: 30,
+        density: Math.pow(10, 6),
+      },
+    ],
+  }),
+  (ctx) => ({
+    goal: {
+      posX: ctx.canvas.width - 180,
+      posY: 140,
+    },
+    planets: [
+      {
+        posX: ctx.canvas.width / 2,
+        posY: 180,
+        radius: 38,
+        density: Math.pow(10, 6) * 1.3,
+      },
+      {
+        posX: ctx.canvas.width / 2,
+        posY: ctx.canvas.height / 2,
+        radius: 40,
+        density: Math.pow(10, 6) * 1.2,
+      },
+      {
+        posX: ctx.canvas.width / 2,
+        posY: ctx.canvas.height - 180,
+        radius: 38,
+        density: Math.pow(10, 6) * 1.3,
+      },
+    ],
+  }),
+  (ctx) => ({
+    goal: {
+      posX: ctx.canvas.width - 250,
+      posY: ctx.canvas.height - 150,
+    },
+    planets: [
+      {
+        posX: 400,
+        posY: ctx.canvas.height / 3,
+        radius: 30,
+        density: Math.pow(10, 7) * 0.6,
+      },
+      {
+        posX: 600,
+        posY: (ctx.canvas.height / 3) * 2,
+        radius: 28,
+        density: Math.pow(10, 7) * 0.8,
+      },
+      {
+        posX: 850,
+        posY: ctx.canvas.height / 2,
+        radius: 36,
+        density: Math.pow(10, 6) * 1.4,
+      },
+      {
+        posX: 1100,
+        posY: ctx.canvas.height / 2 - 120,
+        radius: 26,
+        density: Math.pow(10, 6),
+      },
+    ],
+  }),
+  (ctx) => ({
+    goal: {
+      posX: ctx.canvas.width - 220,
+      posY: ctx.canvas.height / 2,
+    },
+    planets: [
+      { posX: 500, posY: 160, radius: 24, density: Math.pow(10, 7) },
+      { posX: 650, posY: 360, radius: 24, density: Math.pow(10, 7) },
+      { posX: 500, posY: 560, radius: 24, density: Math.pow(10, 7) },
+      { posX: 350, posY: 360, radius: 24, density: Math.pow(10, 7) },
+      {
+        posX: 820,
+        posY: ctx.canvas.height / 2,
+        radius: 40,
+        density: Math.pow(10, 6) * 1.3,
+      },
+    ],
+  }),
+  (ctx) => ({
+    goal: {
+      posX: ctx.canvas.width - 200,
+      posY: 100,
+    },
+    planets: [
+      {
+        posX: ctx.canvas.width / 2,
+        posY: ctx.canvas.height / 2,
+        radius: 70,
+        density: Math.pow(10, 6) * 1.8,
+      },
+      {
+        posX: ctx.canvas.width / 3,
+        posY: ctx.canvas.height / 4,
+        radius: 32,
+        density: Math.pow(10, 6),
+      },
+      {
+        posX: ctx.canvas.width - 450,
+        posY: ctx.canvas.height - 220,
+        radius: 28,
+        density: Math.pow(10, 6) * 1.2,
+      },
+      {
+        posX: ctx.canvas.width / 3,
+        posY: (ctx.canvas.height / 4) * 3,
+        radius: 28,
+        density: Math.pow(10, 6) * 1.1,
+      },
+    ],
+  }),
+  (ctx) => ({
+    goal: {
+      posX: ctx.canvas.width - 220,
+      posY: ctx.canvas.height - 220,
+    },
+    planets: [
+      {
+        posX: ctx.canvas.width / 2,
+        posY: ctx.canvas.height / 2 - 120,
+        radius: 45,
+        density: Math.pow(10, 7) * 0.75,
+      },
+      {
+        posX: ctx.canvas.width / 2,
+        posY: ctx.canvas.height / 2 + 120,
+        radius: 45,
+        density: Math.pow(10, 7) * 0.75,
+      },
+      {
+        posX: ctx.canvas.width / 2 - 220,
+        posY: ctx.canvas.height / 2,
+        radius: 36,
+        density: Math.pow(10, 6) * 1.5,
+      },
+      {
+        posX: ctx.canvas.width / 2 + 220,
+        posY: ctx.canvas.height / 2,
+        radius: 36,
+        density: Math.pow(10, 6) * 1.5,
+      },
+      {
+        posX: ctx.canvas.width - 350,
+        posY: ctx.canvas.height / 2,
+        radius: 30,
+        density: Math.pow(10, 6) * 1.2,
+      },
+    ],
+  }),
+];
 
-const level2 = {
-  goal: { posX: 900, posY: 500 },
-  planets: [
-    { posX: 800, posY: 500, radius: 50, density: Math.pow(10, 6) },
-    { posX: 500, posY: 100, radius: 70, density: Math.pow(10, 6) },
-  ],
-};
+const TOTAL_PLAYABLE_LEVELS = levelDefinitions.length - 1;
 
-const level3 = {
-  goal: { posX: 1000, posY: 500 },
-  planets: [
-    { posX: 800, posY: 500, radius: 10, density: Math.pow(10, 6) },
-    { posX: 500, posY: 300, radius: 30, density: Math.pow(10, 6) },
-  ],
-};
+function getLevelConfiguration(levelIndex, ctx) {
+  const resolver = levelDefinitions[levelIndex];
 
-const level4 = (ctx) => ({
-  goal: { posX: ctx.canvas.width / 2, posY: ctx.canvas.height / 2 },
-  planets: [
-    { posX: 600, posY: 200, radius: 10, density: Math.pow(10, 8) },
-    { posX: 400, posY: 350, radius: 10, density: Math.pow(10, 8) },
-    { posX: 500, posY: 600, radius: 10, density: Math.pow(10, 8) },
-  ],
-});
+  if (!resolver) {
+    return {
+      goal: { posX: 300, posY: 300 },
+      planets: [],
+    };
+  }
+
+  if (typeof resolver === "function") {
+    return resolver(ctx);
+  }
+
+  return resolver;
+}
